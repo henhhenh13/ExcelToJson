3a84bbc0bbb49506d81016e0e7fd301bebff40de
Hoàng Hênh <74145131+henhhenh13@users.noreply.github.com>
2023-02-25 14:30:47 +0000

    chore: naming, clean code, docs

--- index.js
+++ index.js
@@ -2,35 +2,39 @@ const fs = require('fs');
 const XLSX = require('xlsx');
 
 const directoryPath = './languages/locales';
-const dataPath = './data/common.xlsx';
+const sheetPath = './data/common.xlsx';
 
 fs.readdir(directoryPath,  function(err, files) {
-    const workbook = XLSX.readFile(dataPath);
-    const sheet_name_list = workbook.SheetNames;
-    const dataSheets = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-    let languages = {};
 
     if (err) {
         console.error('Error reading directory:', err);
         return;
     }
 
+    const workbook = XLSX.readFile(sheetPath);
+    const sheet_name_list = workbook.SheetNames;
+    const dataSheets = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+    let languages = {};
+
+    // Get languages
     files.forEach((file) => {
         try {
             const data = fs.readFileSync(`${directoryPath}/${file}/common.json`, 'utf8');
             languages[file] = JSON.parse(data);
         } catch (err) {
-            console.log(err);
+            console.log(`Error read file ${file} locale`, err);
         }
     });
 
 
+    // Add new sentenses
     dataSheets.forEach((sheet) => {
         Object.entries(languages).map(([key, value]) => {
             languages[key] = {...languages[key], [sheet.en]: sheet[key]}
         })
     });
 
+    // Export file, overwrite old
     Object.entries(languages).forEach(([key, value]) => {
         try {
             fs.writeFileSync(`${directoryPath}/${key}/common.json`, JSON.stringify(value));
